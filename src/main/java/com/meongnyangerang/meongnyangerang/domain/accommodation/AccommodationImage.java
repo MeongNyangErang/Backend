e14831bd9b16ff2595bc99374af07c9c728a37c5
seokjin <wlsdldos00@gmail.com>
2025-03-25 18:15:36 +0900

    feat: 숙소 이미지 엔티티 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/accommodation/AccommodationImage.java
@@ -0,0 +1,42 @@
+package com.meongnyangerang.meongnyangerang.domain.accommodation;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class AccommodationImage {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "accommodation_id", nullable = false)
+  private Accommodation accommodation;
+
+  @Column(nullable = false)
+  private String imageUrl;
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+}
