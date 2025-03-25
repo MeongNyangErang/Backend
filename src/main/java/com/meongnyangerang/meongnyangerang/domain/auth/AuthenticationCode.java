d6b2e1d940e55263dc754d24510bfc11db21f6d6
Ho-JunYu <hojoon1011@gmail.com>
2025-03-26 02:00:42 +0900

    feat: AuthenticationCode 엔티티 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/auth/AuthenticationCode.java
@@ -0,0 +1,43 @@
+package com.meongnyangerang.meongnyangerang.domain.auth;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class AuthenticationCode {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, length = 100)
+  private String email;
+
+  @Column(nullable = false)
+  private Integer code;
+
+  @Column
+  private Boolean isValid;
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+}
