472f017db2b904b84e1960be1c36f8c73258d8a5
seokjin <wlsdldos00@gmail.com>
2025-03-25 18:20:26 +0900

    feat: 객실 반려동물 편의시설 엔티티 생성 및 객실 반려동물 편의시설 타입 enum 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/room/facility/RoomPetFacilityType.java
@@ -0,0 +1,23 @@
+package com.meongnyangerang.meongnyangerang.domain.room.facility;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum RoomPetFacilityType {
+
+  FOOD_BOWL("식기"),
+  POTTY_SUPPLIES("배변용품"),
+  TOY("장난감"),
+  BED("침대"),
+  DRY_ROOM("드라이룸"),
+  ANTI_SLIP_FLOOR("미끄럼 방지 바닥"),
+  FENCE_AREA("펜스 설치 공간"),
+  CAT_TOWER("캣타워"),
+  CAT_WHEEL("캣휠"),
+  COMB("빗"),
+  PET_STEPS("강아지 계단");
+
+  private final String value;
+}
