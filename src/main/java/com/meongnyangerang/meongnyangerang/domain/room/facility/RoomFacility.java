2c15248b819de3ea823ebb99605e220a3bffa35e
seokjin <wlsdldos00@gmail.com>
2025-03-25 18:19:53 +0900

    feat: 객실 편의시설 엔티티 생성 및 객실 편의시설 enum 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/room/facility/RoomFacility.java
@@ -0,0 +1,47 @@
+package com.meongnyangerang.meongnyangerang.domain.room.facility;
+
+
+import com.meongnyangerang.meongnyangerang.domain.room.Room;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class RoomFacility {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "room_id", nullable = false)
+  private Room room;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private RoomFacilityType type;
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+}
