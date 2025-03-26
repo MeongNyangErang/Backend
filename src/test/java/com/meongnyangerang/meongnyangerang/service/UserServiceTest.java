41db3914c56e9aa443797423ae16c35843850895
Ho-JunYu <hojoon1011@gmail.com>
2025-03-27 04:35:53 +0900

    feat: UserService 중복 이메일 회원가입 실패 테스트 코드 작성

--- src/test/java/com/meongnyangerang/meongnyangerang/service/UserServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/UserServiceTest.java
@@ -1,12 +1,14 @@
 package com.meongnyangerang.meongnyangerang.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.meongnyangerang.meongnyangerang.domain.user.User;
 import com.meongnyangerang.meongnyangerang.dto.UserSignupRequest;
+import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
 import com.meongnyangerang.meongnyangerang.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,7 +25,7 @@ class UserServiceTest {
 
   @Mock
   private UserRepository userRepository;
-  
+
   @Test
   @DisplayName("사용자 회원가입 성공 테스트")
   void registerUserSuccess() {
@@ -37,5 +39,15 @@ class UserServiceTest {
     verify(userRepository).save(any(User.class));
   }
 
+  @Test
+  @DisplayName("중복 이메일 회원가입 실패 테스트")
+  void registerUserDuplicateEmail() {
+    // given
+    when(userRepository.existsByEmail(any())).thenReturn(true);
 
+    // when & then
+    assertThrows(MeongnyangerangException.class, () -> {
+      userService.registerUser(new UserSignupRequest());
+    });
+  }
 }
