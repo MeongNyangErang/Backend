ec7cfc38b9801bfaa4c2b7283d07e5dca51e2ad3
Ho-JunYu <hojoon1011@gmail.com>
2025-03-26 21:17:14 +0900

    feat: 기존 Math.random을 보안 이슈로 SecureRandom으로 변경

--- src/main/java/com/meongnyangerang/meongnyangerang/service/UserService.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/service/UserService.java
@@ -7,6 +7,7 @@ import com.meongnyangerang.meongnyangerang.domain.auth.AuthenticationCode;
 import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
 import com.meongnyangerang.meongnyangerang.repository.AuthenticationCodeRepository;
 import com.meongnyangerang.meongnyangerang.repository.UserRepository;
+import java.security.SecureRandom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,8 +27,8 @@ public class UserService {
       throw new MeongnyangerangException(DUPLICATE_EMAIL);
     }
 
-    // 6자리 랜덤 숫자 생성
-    int code = (int) (Math.random() * 900000) + 100000;
+    // 6자리 랜덤 숫자 생성(Math.random()은 예측 가능한 난수 생성 → 보안 취약 → SecureRandom 사용)
+    int code = 100_000 + new SecureRandom().nextInt(900_000);
 
     authenticationCodeRepository.save(AuthenticationCode.builder()
         .email(email)
