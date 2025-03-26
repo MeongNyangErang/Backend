ca0740fa74e49f32db12a80ad52a5cde671807a9
seokjin <wlsdldos00@gmail.com>
2025-03-27 01:39:27 +0900

    feat: 숙소와 관련된 repository 구현
    - 숙소 repository 구현
    - 숙소 이미지 repository 구현
    - 숙소 편의시설 repository 구현
    - 숙소 반려동물 편의시설 repository 구현
    - 숙소 허용 반려동물 repository 구현

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/accommodation/AccommodationImageRepository.java
@@ -0,0 +1,8 @@
+package com.meongnyangerang.meongnyangerang.repository.accommodation;
+
+import com.meongnyangerang.meongnyangerang.domain.accommodation.AccommodationImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccommodationImageRepository extends JpaRepository<AccommodationImage, Long> {
+
+}
---
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/accommodation/AllowPetRepository.java
@@ -0,0 +1,8 @@
+package com.meongnyangerang.meongnyangerang.repository.accommodation;
+
+import com.meongnyangerang.meongnyangerang.domain.accommodation.AllowPet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AllowPetRepository extends JpaRepository<AllowPet, Long> {
+
+}
