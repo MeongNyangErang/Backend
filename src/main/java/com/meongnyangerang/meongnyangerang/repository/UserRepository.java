19da60ada8daa9f7542f3a6506c73c3fe0f66058
Ho-JunYu <hojoon1011@gmail.com>
2025-03-26 19:07:21 +0900

    chore: User 기본 구조 생성

--- src/main/java/com/meongnyangerang/meongnyangerang/repository/UserRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.meongnyangerang.meongnyangerang.repository;
 
-public interface UserRepository {
+import com.meongnyangerang.meongnyangerang.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/meongnyangerang/meongnyangerang/service/UserService.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/service/UserService.java
@@ -1,5 +1,10 @@
 package com.meongnyangerang.meongnyangerang.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
 
 }
