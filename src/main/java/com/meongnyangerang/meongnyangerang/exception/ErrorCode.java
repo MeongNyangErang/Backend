dafe6acc2d78629b986e374f2b6e0a06afa7be08
seokjin <wlsdldos00@gmail.com>
2025-03-27 21:30:26 +0900

    feat: 파일, AWS S3 관련 에러 코드 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/exception/ErrorCode.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/exception/ErrorCode.java
@@ -15,14 +15,24 @@ public enum ErrorCode {
   INVALID_AUTH_CODE(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다"),
   AUTH_CODE_NOT_FOUND(HttpStatus.BAD_REQUEST, "인증코드를 찾을 수 없습니다. 인증코드 받기를 다시 실행해주세요"),
   USER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 등록된 회원입니다."),
+  INVALID_FILENAME(HttpStatus.BAD_REQUEST, "파일명이 유효하지 않습니다."),
+  INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "파일 확장자가 유효하지 않습니다."),
+  NOT_SUPPORTED_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 파일 형식입니다."),
 
   // 401 UNAUTHORIZED (인증 실패)
 
   // 403 FORBIDDEN (접근 금지)
 
+  // 404  NOT_FOUND
+  FILE_NOT_FOUND(HttpStatus.NOT_FOUND, "파일이 존재하지 않습니다."),
+  FILE_NOT_EMPTY(HttpStatus.NOT_FOUND, "파일이 비어있습니다."),
+
   // 500 INTERNAL SERVER ERROR (서버 내부 오류)
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류가 발생했습니다."),
-  EMAIL_NOT_SEND(HttpStatus.INTERNAL_SERVER_ERROR, "이메일이 정상적으로 전송되지 않았습니다.");
+  EMAIL_NOT_SEND(HttpStatus.INTERNAL_SERVER_ERROR, "이메일이 정상적으로 전송되지 않았습니다."),
+  AMAZON_SERVICE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "아마존 서비스 오류"),
+  INVALID_IO_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 IO 오류"),
+  ;
 
   private final HttpStatus status;
   private final String description;
