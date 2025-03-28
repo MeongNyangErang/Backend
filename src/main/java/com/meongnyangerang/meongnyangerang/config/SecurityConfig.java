e5a1ebbaaa70d6c48ebe3374bb5662b2192cf446
Ho-JunYu <hojoon1011@gmail.com>
2025-03-29 00:36:49 +0900

    feat: SecurityConfig JWT 설정에 맞게 수정 구현

--- src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
@@ -1,31 +1,36 @@
 package com.meongnyangerang.meongnyangerang.config;
 
-import com.meongnyangerang.meongnyangerang.security.UserDetailsServiceImpl;
+import com.meongnyangerang.meongnyangerang.jwt.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
-  private final UserDetailsServiceImpl userDetailsService;
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+        // 세션 비활성화(세션을 사용하지 않고 JWT 인증 방식 사용)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
             // 인증 없이 접근 허용할 엔드포인트
             .requestMatchers(
@@ -34,13 +39,22 @@ public class SecurityConfig {
                 "/api/v1/users/signup",
                 "/api/v1/hosts/signup"
             ).permitAll()
-            .requestMatchers("/api/v1/users/**").hasRole("ROLE_USER")
-            .requestMatchers("/api/v1/hosts/**").hasRole("ROLE_HOST")
-            .requestMatchers("/api/v1/admin/**").hasRole("ROLE_ADMIN")
+            .requestMatchers("/api/v1/users/**").hasAuthority("ROLE_USER")
+            .requestMatchers("/api/v1/hosts/**").hasAuthority("ROLE_HOST")
+            .requestMatchers("/api/v1/admin/**").hasAuthority("ROLE_ADMIN")
             .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
         )
-        .formLogin(Customizer.withDefaults()) // 기본 로그인 폼 사용(추후 JWT 도입 시 제거 예정)
-        .logout(LogoutConfigurer::permitAll // 모든 사용자가 로그아웃 가능하도록 허용(일시)
+        .addFilterBefore(jwtAuthenticationFilter,
+            UsernamePasswordAuthenticationFilter.class) // JWT 필터 추가(로그인한 사용자의 JWT를 검증하여 인증된 사용자로 설정)
+        // 예외 처리 추가(추후 더 자세하게 수정 예정)
+        .exceptionHandling(ex -> ex
+            .authenticationEntryPoint((request, response, authException) -> {
+              response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                  "Unauthorized");  // 인증 실패(401)
+            })
+            .accessDeniedHandler((request, response, accessDeniedException) -> {
+              response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");  // 권한 부족(403)
+            })
         );
     return http.build();
   }
@@ -56,11 +70,4 @@ public class SecurityConfig {
     return new BCryptPasswordEncoder();
   }
 
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService);
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
-  }
 }
