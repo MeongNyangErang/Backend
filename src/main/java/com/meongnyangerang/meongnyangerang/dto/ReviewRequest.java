0d175428a875e2fde611f6bb06a5f9b7d0c0fa69
JJae <jjae.3.lee@gmail.com>
2025-03-30 05:41:15 +0900

    feat: ReviewRequest 작성, ReivewRepository 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/ReviewRequest.java
@@ -0,0 +1,46 @@
+package com.meongnyangerang.meongnyangerang.dto;
+
+import com.meongnyangerang.meongnyangerang.domain.accommodation.Accommodation;
+import com.meongnyangerang.meongnyangerang.domain.reservation.Reservation;
+import com.meongnyangerang.meongnyangerang.domain.review.Review;
+import com.meongnyangerang.meongnyangerang.domain.user.User;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Builder
+public class ReviewRequest {
+
+  @NotNull
+  private Long reservationId;
+
+  @NotNull
+  @Min(value = 0)
+  @Max(value = 5)
+  private Double userRating;
+
+  @NotNull
+  @Min(value = 0)
+  @Max(value = 5)
+  private Double petFriendlyRating;
+
+  private String content;
+
+  private MultipartFile image;
+
+  public Review toEntity(User user, Accommodation accommodation, Reservation reservation) {
+    return Review.builder()
+        .user(user)
+        .accommodation(accommodation)
+        .reservation(reservation)
+        .content(content)
+        .userRating(userRating)
+        .petFriendlyRating(petFriendlyRating)
+        .reportCount(0)
+        .build();
+  }
+}
