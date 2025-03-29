a43db3b5fad4c2f5bb83c899c78e4041a16baa11
seokjin <wlsdldos00@gmail.com>
2025-03-29 16:55:09 +0900

    feat: 각 Repository에 숙소 ID로 편의시설, 반려동물 편의시설, 허용 반려동물을 조회하는 메서드 정의 및 @Repository 어노테이션 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/repository/accommodation/AllowPetRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/accommodation/AllowPetRepository.java
@@ -1,8 +1,12 @@
 package com.meongnyangerang.meongnyangerang.repository.accommodation;
 
 import com.meongnyangerang.meongnyangerang.domain.accommodation.AllowPet;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AllowPetRepository extends JpaRepository<AllowPet, Long> {
 
+  List<AllowPet> findAllByAccommodationId(Long accommodationId);
 }
