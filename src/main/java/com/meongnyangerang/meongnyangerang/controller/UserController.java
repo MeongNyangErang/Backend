89f1e43907370bcab7cdcbb24d720b812da2e48e
JJae <jjae.3.lee@gmail.com>
2025-03-30 01:25:23 +0900

    feat: UserController에 예약 목록 조회 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/UserController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/UserController.java
@@ -1,16 +1,21 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
+import com.meongnyangerang.meongnyangerang.domain.reservation.ReservationStatus;
+import com.meongnyangerang.meongnyangerang.dto.CustomReservationResponse;
 import com.meongnyangerang.meongnyangerang.dto.ReservationRequest;
 import com.meongnyangerang.meongnyangerang.dto.UserSignupRequest;
 import com.meongnyangerang.meongnyangerang.service.ReservationService;
 import com.meongnyangerang.meongnyangerang.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -39,4 +44,17 @@ public class UserController {
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
+  @GetMapping("/reservations")
+  public ResponseEntity<CustomReservationResponse> getReservations(
+//      @AuthenticationPrincipal CustomUserDetails userDetails,
+      @RequestParam(defaultValue = "0") Long cursorId,
+      @RequestParam(defaultValue = "20") @Range(min = 1, max = 100) int size,
+      @RequestParam ReservationStatus status) {
+
+    // 로그인된 사용자의 ID를 받아서 예약을 생성 (Security 적용 후 userDetails.getId())
+    CustomReservationResponse response = reservationService.findByStatus(1L, cursorId, size,
+        status);
+
+    return ResponseEntity.ok(response);
+  }
 }
