a6e13bd899904a83abd26c1e570f23c3b0fa6f8c
seokjin <wlsdldos00@gmail.com>
2025-03-29 16:54:18 +0900

    feat: 숙소 조회 엔드포인트 구현 및 불필요한 주석 제거

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationController.java
@@ -1,12 +1,14 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
 import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationCreateRequest;
+import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationResponse;
 import com.meongnyangerang.meongnyangerang.service.AccommodationService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -22,8 +24,6 @@ public class AccommodationController {
 
   /**
    * 숙소 등록 API 숙소 정보와 이미지 파일을 함께 등록
-   *
-   * @return 201
    */
   // TODO: UserDetails 구현이 완료되면 주석 해제
   // TODO: host만 호출할 수 있도록 수정
@@ -36,5 +36,17 @@ public class AccommodationController {
   ) {
     accommodationService.createAccommodation(1L, request, thumbnail, additionalImages);
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  /**
+   * 숙소 조회 API
+   */
+  // TODO: UserDetails 구현이 완료되면 주석 해제
+  // TODO: host만 호출할 수 있도록 수정
+  @GetMapping
+  public ResponseEntity<AccommodationResponse> getAccommodation(
+      //@AuthenticationPrincipal UserDetail userDetail
+  ){
+    return ResponseEntity.ok(accommodationService.getAccommodation(1L));
   }
 }
