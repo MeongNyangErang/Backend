dc2f17c1d358f972d777bceb7343c724916338d2
Ho-JunYu <hojoon1011@gmail.com>
2025-03-31 03:11:40 +0900

    feat: 사용자 회원가입 성공 테스트 - 프로필 이미지 포함

--- src/test/java/com/meongnyangerang/meongnyangerang/service/UserServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/UserServiceTest.java
@@ -11,12 +11,14 @@ import com.meongnyangerang.meongnyangerang.dto.UserSignupRequest;
 import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
 import com.meongnyangerang.meongnyangerang.repository.UserRepository;
 import com.meongnyangerang.meongnyangerang.service.image.ImageService;
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,5 +53,25 @@ class UserServiceTest {
     verify(userRepository).save(any(User.class));
   }
 
+  @Test
+  @DisplayName("사용자 회원가입 성공 테스트 - 프로필 이미지 포함")
+  void registerUserSuccessWithImage() throws IOException {
+    // given
+    UserSignupRequest request = new UserSignupRequest();
+    request.setEmail("user@example.com");
+    request.setNickname("nickname");
+    request.setPassword("password123!");
 
+    MockMultipartFile imageFile = new MockMultipartFile(
+        "profileImage", "test.png", "image/png", "dummy".getBytes()
+    );
+
+    when(userRepository.existsByEmail(any())).thenReturn(false);
+    when(passwordEncoder.encode(any())).thenReturn("encodedPassword");
+    when(imageService.storeImage(any())).thenReturn("https://s3.bucket/image/test.png");
+
+    // when & then
+    assertDoesNotThrow(() -> userService.registerUser(request, imageFile));
+    verify(userRepository).save(any(User.class));
+  }
 }
