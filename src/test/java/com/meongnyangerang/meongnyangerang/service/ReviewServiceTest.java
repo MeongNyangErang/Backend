0cfe682c7b8b9f6b70bfa57d8905210f48d48871
JJae <jjae.3.lee@gmail.com>
2025-03-30 20:21:54 +0900

    refactor: 이미지 등록 추가로 인해 테스트 코드 수정

--- src/test/java/com/meongnyangerang/meongnyangerang/service/ReviewServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/ReviewServiceTest.java
@@ -8,22 +8,30 @@ import static org.mockito.Mockito.when;
 
 import com.meongnyangerang.meongnyangerang.domain.reservation.Reservation;
 import com.meongnyangerang.meongnyangerang.domain.reservation.ReservationStatus;
+import com.meongnyangerang.meongnyangerang.domain.review.Review;
+import com.meongnyangerang.meongnyangerang.domain.review.ReviewImage;
 import com.meongnyangerang.meongnyangerang.domain.room.Room;
 import com.meongnyangerang.meongnyangerang.domain.user.User;
 import com.meongnyangerang.meongnyangerang.dto.ReviewRequest;
 import com.meongnyangerang.meongnyangerang.exception.ErrorCode;
 import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
 import com.meongnyangerang.meongnyangerang.repository.ReservationRepository;
+import com.meongnyangerang.meongnyangerang.repository.ReviewImageRepository;
 import com.meongnyangerang.meongnyangerang.repository.ReviewRepository;
+import com.meongnyangerang.meongnyangerang.service.image.ImageService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 class ReviewServiceTest {
@@ -33,6 +41,12 @@ class ReviewServiceTest {
 
   @Mock
   private ReviewRepository reviewRepository;
+
+  @Mock
+  private ReviewImageRepository imageRepository;
+
+  @Mock
+  private ImageService imageService;
 
   @InjectMocks
   private ReviewService reviewService;
@@ -64,18 +78,40 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
+    String url = "https://test.com/images/image.jpg";
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.of(reservation));
     when(reviewRepository.existsByUserIdAndReservationId(user.getId(),
         request.getReservationId())).thenReturn(false);
+    when(imageService.storeImage(images.get(0))).thenReturn(url);
 
     // when
-    reviewService.createReview(user.getId(), request);
+    reviewService.createReview(user.getId(), request, images);
 
     // then
+    ArgumentCaptor<ReviewImage> imageCaptor = ArgumentCaptor.forClass(ReviewImage.class);
+    verify(imageRepository, times(1)).save(imageCaptor.capture());
+
+    ArgumentCaptor<Review> reviewCaptor = ArgumentCaptor.forClass(Review.class);
+    verify(reviewRepository, times(1)).save(reviewCaptor.capture());
+
     verify(reservationRepository, times(1)).findById(request.getReservationId());
     verify(reviewRepository, times(1)).existsByUserIdAndReservationId(user.getId(),
         request.getReservationId());
+
+    ReviewImage capturedReviewImage = imageCaptor.getValue();
+    assertEquals(url, capturedReviewImage.getImageUrl());
+    assertEquals(user, capturedReviewImage.getReview().getUser());
+
+    Review capturedReview = reviewCaptor.getValue();
+    assertEquals(3.0, capturedReview.getUserRating());
+    assertEquals(4.0, capturedReview.getPetFriendlyRating());
+    assertEquals("반려동물이 즐거워해요", capturedReview.getContent());
   }
 
   @Test
@@ -105,12 +141,16 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.empty());
 
     // when
     MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
-      reviewService.createReview(user.getId(), request);
+      reviewService.createReview(user.getId(), request, images);
     });
 
     // then
@@ -144,6 +184,10 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.of(reservation));
     when(reviewRepository.existsByUserIdAndReservationId(user.getId(), request.getReservationId()))
@@ -151,7 +195,7 @@ class ReviewServiceTest {
 
     // when
     MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
-      reviewService.createReview(user.getId(), request);
+      reviewService.createReview(user.getId(), request, images);
     });
 
     // then
@@ -185,6 +229,10 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.of(reservation));
     when(reviewRepository.existsByUserIdAndReservationId(100L, request.getReservationId()))
@@ -192,7 +240,7 @@ class ReviewServiceTest {
 
     // when
     MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
-      reviewService.createReview(100L, request);
+      reviewService.createReview(100L, request, images);
     });
 
     // then
@@ -226,6 +274,10 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.of(reservation));
     when(reviewRepository.existsByUserIdAndReservationId(user.getId(), request.getReservationId()))
@@ -233,7 +285,7 @@ class ReviewServiceTest {
 
     // when
     MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
-      reviewService.createReview(user.getId(), request);
+      reviewService.createReview(user.getId(), request, images);
     });
 
     // then
@@ -267,6 +319,10 @@ class ReviewServiceTest {
         .content("반려동물이 즐거워해요")
         .build();
 
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image", "image.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
     when(reservationRepository.findById(request.getReservationId())).thenReturn(
         Optional.of(reservation));
     when(reviewRepository.existsByUserIdAndReservationId(user.getId(), request.getReservationId()))
@@ -274,10 +330,58 @@ class ReviewServiceTest {
 
     // when
     MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
-      reviewService.createReview(user.getId(), request);
+      reviewService.createReview(user.getId(), request, images);
     });
 
     // then
     assertEquals(ErrorCode.REVIEW_CREATION_NOT_ALLOWED, e.getErrorCode());
+  }
+
+  @Test
+  @DisplayName("이미지 개수가 3개를 초과한 경우, MAX_IMAGE_LIMIT_EXCEEDED 예외가 발생해야 한다.")
+  void createReview_review_max_image_limit_exceeded() {
+    // given
+    User user = User.builder().id(1L).build();
+    Room room = Room.builder().id(1L).build();
+
+    Reservation reservation = Reservation.builder()
+        .id(1L)
+        .status(ReservationStatus.RESERVED)
+        .user(user)
+        .room(room)
+        .checkInDate(LocalDate.of(2025, 3, 30))
+        .checkOutDate(LocalDate.of(2025, 4, 1))
+        .peopleCount(2)
+        .petCount(1)
+        .totalPrice(30000L)
+        .createdAt(LocalDateTime.now())
+        .build();
+
+    ReviewRequest request = ReviewRequest.builder()
+        .reservationId(1L)
+        .userRating(3.0)
+        .petFriendlyRating(4.0)
+        .content("반려동물이 즐거워해요")
+        .build();
+
+    List<MultipartFile> images = List.of(
+        new MockMultipartFile("image1", "image1.jpg", "image/jpeg", new byte[]{1, 2, 3, 4}),
+        new MockMultipartFile("image2", "image2.jpg", "image/jpeg", new byte[]{1, 2, 3, 4}),
+        new MockMultipartFile("image3", "image3.jpg", "image/jpeg", new byte[]{1, 2, 3, 4}),
+        new MockMultipartFile("image4", "image4.jpg", "image/jpeg", new byte[]{1, 2, 3, 4})
+    );
+
+    when(reservationRepository.findById(request.getReservationId())).thenReturn(
+        Optional.of(reservation));
+    when(reviewRepository.existsByUserIdAndReservationId(user.getId(), request.getReservationId()))
+        .thenReturn(false);
+
+    // when
+    MeongnyangerangException e = assertThrows(MeongnyangerangException.class, () -> {
+      reviewService.createReview(user.getId(), request, images);
+    });
+
+    // then
+    assertEquals(ErrorCode.MAX_IMAGE_LIMIT_EXCEEDED, e.getErrorCode());
   }
 }
