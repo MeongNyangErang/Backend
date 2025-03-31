48227dc6e519d6bbb7218c01d98e886097b6a21e
seokjin <wlsdldos00@gmail.com>
2025-04-01 01:22:50 +0900

    faet: 숙소 ID, 커서, Pageable을 받아 숙소를 조회하고 Pageable에 정의된 기준(id)으로 정렬하고 limit 개수만큼 반환

--- src/main/java/com/meongnyangerang/meongnyangerang/repository/RoomRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/RoomRepository.java
@@ -1,10 +1,22 @@
 package com.meongnyangerang.meongnyangerang.repository;
 
 import com.meongnyangerang.meongnyangerang.domain.room.Room;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
+  @Query("SELECT r FROM Room r " +
+      "WHERE r.accommodation.id = :accommodationId " +
+      "AND (:cursorId IS NULL OR r.id < :cursorId) ")
+  List<Room> findRoomsWithCursor(
+      @Param("accommodationId") Long accommodationId,
+      @Param("cursorId") Long cursorId,
+      Pageable pageable
+  );
 }
