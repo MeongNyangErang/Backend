cd6edf49476752421fae630babc8e3fa8235bf27
Ho-JunYu <hojoon1011@gmail.com>
2025-04-01 02:27:36 +0900

    feat: AccountCleanupServiceTest 테스트 코드 수정 - any제외

--- src/test/java/com/meongnyangerang/meongnyangerang/service/AccountCleanupServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/AccountCleanupServiceTest.java
@@ -11,6 +11,7 @@ import com.meongnyangerang.meongnyangerang.domain.user.User;
 import com.meongnyangerang.meongnyangerang.domain.user.UserStatus;
 import com.meongnyangerang.meongnyangerang.repository.HostRepository;
 import com.meongnyangerang.meongnyangerang.repository.UserRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,16 +35,21 @@ class AccountCleanupServiceTest {
   @Test
   @DisplayName("30일 경과된 사용자 및 호스트 하드 삭제")
   void deleteExpiredSoftDeletedAccounts() {
+    // given
+    LocalDateTime cutoff = LocalDateTime.now().minusDays(30);
+
     List<User> expiredUsers = List.of(User.builder().id(1L).build());
     List<Host> expiredHosts = List.of(Host.builder().id(1L).build());
 
-    when(userRepository.findAllByStatusAndDeletedAtBefore(eq(UserStatus.DELETED), any()))
+    when(userRepository.findAllByStatusAndDeletedAtBefore(UserStatus.DELETED, cutoff))
         .thenReturn(expiredUsers);
-    when(hostRepository.findAllByStatusAndDeletedAtBefore(eq(HostStatus.DELETED), any()))
+    when(hostRepository.findAllByStatusAndDeletedAtBefore(HostStatus.DELETED, cutoff))
         .thenReturn(expiredHosts);
 
+    // when
     accountCleanupService.deleteExpiredSoftDeletedUsers();
 
+    // then
     verify(userRepository).deleteAll(expiredUsers);
     verify(hostRepository).deleteAll(expiredHosts);
   }
