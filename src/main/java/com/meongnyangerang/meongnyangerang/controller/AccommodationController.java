a8393a0c55d6c75732e055a6a44c8d7b59b7f9b4
seokjin <wlsdldos00@gmail.com>
2025-04-01 02:47:34 +0900

    feat: User 정보 활성화

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationController.java
@@ -3,12 +3,14 @@ package com.meongnyangerang.meongnyangerang.controller;
 import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationCreateRequest;
 import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationResponse;
 import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationUpdateRequest;
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
 import com.meongnyangerang.meongnyangerang.service.AccommodationService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,44 +29,40 @@ public class AccommodationController {
   /**
    * 숙소 등록 API 숙소 정보와 이미지 파일을 함께 등록
    */
-  // TODO: UserDetails 구현이 완료되면 주석 해제
-  // TODO: host만 호출할 수 있도록 수정
   @PostMapping
   public ResponseEntity<Void> createAccommodation(
-      //@AuthenticationPrincipal UserDetail userDetail,
+      @AuthenticationPrincipal UserDetailsImpl userDetail,
       @Valid @RequestPart AccommodationCreateRequest request,
       @RequestPart MultipartFile thumbnail,
       @RequestPart(required = false) List<MultipartFile> additionalImages
   ) {
-    accommodationService.createAccommodation(1L, request, thumbnail, additionalImages);
+    accommodationService.createAccommodation(
+        userDetail.getId(), request, thumbnail, additionalImages);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
   /**
    * 숙소 조회 API
    */
-  // TODO: UserDetails 구현이 완료되면 주석 해제
-  // TODO: host만 호출할 수 있도록 수정
   @GetMapping
   public ResponseEntity<AccommodationResponse> getAccommodation(
-      //@AuthenticationPrincipal UserDetail userDetail
-  ){
-    return ResponseEntity.ok(accommodationService.getAccommodation(1L));
+      @AuthenticationPrincipal UserDetailsImpl userDetail
+  ) {
+    return ResponseEntity.ok(accommodationService.getAccommodation(userDetail.getId()));
   }
 
   /**
    * 숙소 수정 API
    */
-  // TODO: UserDetails 구현이 완료되면 주석 해제
-  // TODO: host만 호출할 수 있도록 수정
   @PutMapping
   public ResponseEntity<AccommodationResponse> updateAccommodation(
-      //@AuthenticationPrincipal UserDetail userDetail,
+      @AuthenticationPrincipal UserDetailsImpl userDetail,
       @Valid @RequestPart AccommodationUpdateRequest request,
       @RequestPart(required = false) MultipartFile newThumbnail,
       @RequestPart(required = false) List<MultipartFile> newAdditionalImages
-  ){
+  ) {
     return ResponseEntity.ok(
-        accommodationService.updateAccommodation(1L, request, newThumbnail, newAdditionalImages));
+        accommodationService.updateAccommodation(
+            userDetail.getId(), request, newThumbnail, newAdditionalImages));
   }
 }
