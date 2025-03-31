259c45bba6dc06d7c7f57fd167f35e568ccc0c75
JJae <jjae.3.lee@gmail.com>
2025-03-31 22:35:44 +0900

    feat: ReviewController 에 내 리뷰 삭제 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/ReviewController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/ReviewController.java
@@ -13,6 +13,7 @@ import org.hibernate.validator.constraints.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,5 +66,14 @@ public class ReviewController {
         accommodationId, cursor, size);
 
     return ResponseEntity.ok(response);
+  }
+
+  @DeleteMapping("/users/reviews/{reviewId}")
+  public ResponseEntity<Void> deleteReview(@PathVariable Long reviewId,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+    reviewService.deleteReview(reviewId, userDetails.getId());
+
+    return ResponseEntity.ok().build();
   }
 }
