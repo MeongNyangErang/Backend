897e7f51a3924c1982d5f8fb961d001a7d6c1c3a
Ho-JunYu <hojoon1011@gmail.com>
2025-04-02 03:30:11 +0900

    feat:찜 삭제 API 컨트롤러 구현

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +27,11 @@ public class WishlistController {
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
+  // 찜 삭제 API
+  @DeleteMapping("/{accommodationId}")
+  public ResponseEntity<Void> removeWishlist(@AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long accommodationId) {
+    wishlistService.removeWishlist(userDetails.getId(), accommodationId);
+    return ResponseEntity.noContent().build();
+  }
 }
