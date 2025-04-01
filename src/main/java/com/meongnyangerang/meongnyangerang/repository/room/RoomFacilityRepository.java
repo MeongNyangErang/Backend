903e981c39d741c876465c6aeaf6acef7a9754b4
seokjin <wlsdldos00@gmail.com>
2025-04-02 00:02:59 +0900

    feat: 각 데이터를 roomId 기반으로 삭제하는 벌크연산 쿼리 구현

--- src/main/java/com/meongnyangerang/meongnyangerang/repository/room/RoomFacilityRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/room/RoomFacilityRepository.java
@@ -3,10 +3,16 @@ package com.meongnyangerang.meongnyangerang.repository.room;
 import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomFacility;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomFacilityRepository extends JpaRepository<RoomFacility, Long> {
 
   List<RoomFacility> findAllByRoomId(Long roomId);
+
+  @Modifying
+  @Query("DELETE FROM RoomFacility f WHERE f.room.id = :roomId")
+  void deleteAllByRoomId(Long roomId);
 }
--- src/main/java/com/meongnyangerang/meongnyangerang/repository/room/RoomPetFacilityRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/room/RoomPetFacilityRepository.java
@@ -3,10 +3,16 @@ package com.meongnyangerang.meongnyangerang.repository.room;
 import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomPetFacility;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomPetFacilityRepository extends JpaRepository<RoomPetFacility, Long> {
 
   List<RoomPetFacility> findAllByRoomId(Long roomId);
+
+  @Modifying
+  @Query("DELETE FROM RoomPetFacility pf WHERE pf.room.id = :roomId")
+  void deleteAllByRoomId(Long roomId);
 }
