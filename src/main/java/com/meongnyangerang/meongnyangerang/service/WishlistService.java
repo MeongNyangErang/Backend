ade203ecf2ca0bdcb9c3c7430b835a068a253c3b
Ho-JunYu <hojoon1011@gmail.com>
2025-04-02 02:49:17 +0900

    feat: Wishlist 서비스 클래스 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/service/WishlistService.java
@@ -0,0 +1,10 @@
+package com.meongnyangerang.meongnyangerang.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WishlistService {
+
+}
