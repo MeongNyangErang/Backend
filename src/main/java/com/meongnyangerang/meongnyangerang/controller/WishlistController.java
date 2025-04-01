0413ded94df599dbfc17921465d62db8742a5275
Ho-JunYu <hojoon1011@gmail.com>
2025-04-02 02:54:46 +0900

    feat:찜 등록 API 컨트롤러 구현

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
@@ -1,12 +1,29 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
+import com.meongnyangerang.meongnyangerang.service.WishlistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users/wishlist")
+@RequestMapping("/api/v1/users/wishlist/accommodations")
 public class WishlistController {
+
+  private final WishlistService wishlistService;
+
+  // 찜 등록 API
+  @PostMapping("/{accommodationId}")
+  public ResponseEntity<Void> addWishlist(@AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable Long accommodationId) {
+    wishlistService.addWishlist(userDetails.getId(), accommodationId);
+    return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
 
 }
