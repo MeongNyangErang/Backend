9df5c60fe3dec868211f7e6ff43c4f6df8055c66
seokjin <wlsdldos00@gmail.com>
2025-04-01 19:45:14 +0900

    feat: 객실 상세 조회 응답값을 반환하는 record 구현
    - enum 값은 value(한글 버전)으로 변환하여 반환

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/room/RoomResponse.java
@@ -0,0 +1,58 @@
+package com.meongnyangerang.meongnyangerang.dto.room;
+
+import com.meongnyangerang.meongnyangerang.domain.room.Room;
+import com.meongnyangerang.meongnyangerang.domain.room.facility.Hashtag;
+import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomFacility;
+import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomPetFacility;
+import java.time.LocalTime;
+import java.util.List;
+
+public record RoomResponse(
+    String name,
+    String description,
+    Integer standardPeopleCount,
+    Integer maxPeopleCount,
+    Integer standardPetCount,
+    Integer maxPetCount,
+    Long price,
+    Long extraPeopleFee,
+    Long extraPetFee,
+    Long extraFee,
+    LocalTime checkInTime,
+    LocalTime checkOutTime,
+    String imageUrl,
+    List<String> facilityTypes,
+    List<String> petFacilityTypes,
+    List<String> hashtagTypes
+) {
+
+  public static RoomResponse of(
+      Room room,
+      List<RoomFacility> facilities,
+      List<RoomPetFacility> petFacilities,
+      List<Hashtag> hashtags
+  ) {
+    List<String> facilityValues = facilities.stream()
+        .map(facility -> facility.getType().getValue())
+        .toList();
+
+    List<String> petFacilityValues = petFacilities.stream()
+        .map(petFacility -> petFacility.getType().getValue())
+        .toList();
+
+    List<String> hashtagsValues = hashtags.stream()
+        .map(hashtag -> hashtag.getType().getValue())
+        .toList();
+
+    return new RoomResponse(
+        room.getName(), room.getDescription(),
+        room.getStandardPeopleCount(), room.getMaxPeopleCount(),
+        room.getStandardPetCount(), room.getMaxPetCount(),
+        room.getPrice(), room.getExtraPeopleFee(),
+        room.getExtraPetFee(), room.getExtraFee(),
+        room.getCheckInTime(), room.getCheckOutTime(),
+        room.getImageUrl(), facilityValues,
+        petFacilityValues, hashtagsValues
+    );
+  }
+}
