3c34a3fedcb0fe6f69c00ae3bef3b9f87f472c39
Ho-JunYu <hojoon1011@gmail.com>
2025-04-02 21:00:25 +0900

    feat:Wishlist 목록 조회 API 응답을 위한 DTO 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/WishlistResponse.java
@@ -0,0 +1,19 @@
+package com.meongnyangerang.meongnyangerang.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class WishlistResponse {
+
+  private Long wishlistId;
+  private Long accommodationId;
+  private String accommodationName;
+  private String thumbnailImageUrl;
+  private String address;
+  private Double petScore;
+  private Double userScore;
+}
