5fda2580b5bd3e923d51b3b5e77579ce6bae9aec
Ho-JunYu <hojoon1011@gmail.com>
2025-04-02 20:52:45 +0900

    feat:찜 조회 API 컨트롤러 생성

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/WishlistController.java
@@ -3,13 +3,16 @@ package com.meongnyangerang.meongnyangerang.controller;
 import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
 import com.meongnyangerang.meongnyangerang.service.WishlistService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,4 +37,15 @@ public class WishlistController {
     wishlistService.removeWishlist(userDetails.getId(), accommodationId);
     return ResponseEntity.noContent().build();
   }
+
+  // 찜 조회 API
+  @GetMapping
+  public ResponseEntity<CustomWishlistResponse<WishlistResponse>> getUserWishlist(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestParam(defaultValue = "0") Long cursor,
+      @RequestParam(defaultValue = "20") @Range(min = 1, max = 100) int size) {
+
+    return ResponseEntity.ok(wishlistService.getUserWishlists(userDetails.getId(), cursor, size));
+  }
+
 }
