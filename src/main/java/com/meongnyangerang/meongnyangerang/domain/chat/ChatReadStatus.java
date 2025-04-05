49da2190ae4547c61f3548b50699026b802be080
seokjin <wlsdldos00@gmail.com>
2025-04-06 01:09:46 +0900

    feat: 채팅방 읽은 상태를 관리하는 엔티티 구현

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/chat/ChatReadStatus.java
@@ -0,0 +1,42 @@
+package com.meongnyangerang.meongnyangerang.domain.chat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class ChatReadStatus {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private Long chatRoomId;
+
+  @Column(nullable = false)
+  private Long participantId;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private SenderType participantType;
+
+  @Column(nullable = false)
+  private LocalDateTime lastReadTime;
+}
