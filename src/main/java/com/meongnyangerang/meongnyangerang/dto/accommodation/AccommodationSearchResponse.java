6dc47bab95c90da0831b1e44e41a2184af3ef608
Ho-JunYu <hojoon1011@gmail.com>
2025-04-07 02:15:19 +0900

    feat: AccommodationSearchResponse에 fromDocument 메서드 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/accommodation/AccommodationSearchResponse.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/accommodation/AccommodationSearchResponse.java
@@ -1,11 +1,14 @@
 package com.meongnyangerang.meongnyangerang.dto.accommodation;
 
+import com.meongnyangerang.meongnyangerang.domain.AccommodationRoomDocument;
 import com.meongnyangerang.meongnyangerang.domain.accommodation.AccommodationType;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class AccommodationSearchResponse {
 
   private Long accommodationId;
@@ -17,4 +20,16 @@ public class AccommodationSearchResponse {
   private Long price;
   private AccommodationType accommodationType;
 
+  public static AccommodationSearchResponse fromDocument(AccommodationRoomDocument doc) {
+    return AccommodationSearchResponse.builder()
+        .accommodationId(doc.getAccommodationId())
+        .accommodationName(doc.getAccommodationName())
+        .address(doc.getAddress())
+        .detailedAddress(doc.getDetailedAddress)
+        .thumbnailUrl(doc.getThumbanilUrl)
+        .totalRating(doc.getTotalRating())
+        .price(doc.getPrice())
+        .accommodationType(doc.getAccommodationType())
+        .build();
+  }
 }
