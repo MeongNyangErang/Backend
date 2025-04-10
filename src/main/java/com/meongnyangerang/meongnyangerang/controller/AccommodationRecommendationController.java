41801d1e7c9886fa75337dcbcc45309cab9ca86c
JJae <jjae.3.lee@gmail.com>
2025-04-10 10:36:02 +0900

    feat: AccommodationRecommendationController 에 맞춤 숙소 API 추가
    - 사용자가 등록한 반려동물 정보 바탕으로 추천

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationRecommendationController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/AccommodationRecommendationController.java
@@ -1,11 +1,13 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
-import com.meongnyangerang.meongnyangerang.dto.accommodation.DefaultRecommendationResponse;
+import com.meongnyangerang.meongnyangerang.dto.accommodation.RecommendationResponse;
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
 import com.meongnyangerang.meongnyangerang.service.AccommodationRecommendationService;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +21,16 @@ public class AccommodationRecommendationController {
 
   // 비로그인 사용자 기본 추천
   @GetMapping("/default")
-  public ResponseEntity<Map<String, List<DefaultRecommendationResponse>>> getDefaultRecommendations() {
+  public ResponseEntity<Map<String, List<RecommendationResponse>>> getDefaultRecommendations() {
 
     return ResponseEntity.ok(recommendationService.getDefaultRecommendations());
+  }
+
+  // 사용자가 등록한 반려동물 기반 추천
+  @GetMapping("/user-pet")
+  public ResponseEntity<Map<String, List<RecommendationResponse>>> getUserPetRecommendations(
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+    return ResponseEntity.ok(recommendationService.getUserPetRecommendations(userDetails.getId()));
   }
 }
