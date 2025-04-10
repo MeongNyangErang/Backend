5bdd84373c695d6cae45dfa2ce19e28488dade06
seokjin <wlsdldos00@gmail.com>
2025-04-10 23:56:06 +0900

    faet: 알림 수신자의 정보를 담을 record 구현

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/notification/NotificationReceiverInfo.java
@@ -0,0 +1,14 @@
+package com.meongnyangerang.meongnyangerang.dto.notification;
+
+import com.meongnyangerang.meongnyangerang.domain.chat.SenderType;
+import com.meongnyangerang.meongnyangerang.domain.host.Host;
+import com.meongnyangerang.meongnyangerang.domain.user.User;
+
+public record NotificationReceiverInfo(
+    Long receiverId,
+    SenderType receiverType,
+    User user,
+    Host host
+) {
+
+}
