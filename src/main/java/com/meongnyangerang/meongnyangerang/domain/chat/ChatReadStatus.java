c0d968dfdb0a74e2e6cccdc0f1584958e24fe07b
seokjin <wlsdldos00@gmail.com>
2025-04-10 23:52:23 +0900

    feat: ChatReadStatus에 ChatRoom과의 연관관계 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/domain/chat/ChatReadStatus.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/chat/ChatReadStatus.java
@@ -5,9 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,8 +30,9 @@ public class ChatReadStatus {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(nullable = false)
-  private Long chatRoomId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "chat_room_id", nullable = false)
+  private ChatRoom chatRoom;
 
   @Column(nullable = false)
   private Long participantId;
