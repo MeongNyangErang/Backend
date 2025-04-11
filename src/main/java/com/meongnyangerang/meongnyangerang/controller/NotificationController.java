e1105bd7e8ea0882da9e73fca3200309ff2e93c7
seokjin <wlsdldos00@gmail.com>
2025-04-12 03:33:53 +0900

    feat: 메시지 알림 전송 API 컨트롤러 구현

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/NotificationController.java
@@ -0,0 +1,34 @@
+package com.meongnyangerang.meongnyangerang.controller;
+
+import com.meongnyangerang.meongnyangerang.dto.notification.MessageNotificationRequest;
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
+import com.meongnyangerang.meongnyangerang.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/notifications")
+public class NotificationController {
+
+  private final NotificationService notificationService;
+
+  /**
+   * 메시지 알림 전송
+   */
+  @PostMapping("/messages")
+  public ResponseEntity<Void> sendMessageNotification(
+      @RequestBody MessageNotificationRequest request,
+      @AuthenticationPrincipal UserDetailsImpl userDetail
+  ) {
+    notificationService.sendNotification(request, userDetail.getId());
+    return ResponseEntity.ok().build();
+  }
+}
