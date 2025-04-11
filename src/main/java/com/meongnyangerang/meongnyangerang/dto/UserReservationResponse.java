b45a70a1306e68188a590d77512f912bc49f1c3c
JJae <jjae.3.lee@gmail.com>
2025-04-11 21:58:36 +0900

    feat: UserReservationResponse에 reservatoinId 반환 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/UserReservationResponse.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/UserReservationResponse.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Builder
 public class UserReservationResponse {
 
+  private Long reservationId;
   private String reservationDate;
   private String accommodationName;
   private String roomName;
