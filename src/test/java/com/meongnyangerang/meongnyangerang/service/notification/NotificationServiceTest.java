eacdb7ff69de7111234b34ba1cc84bc59697e1ba
seokjin <wlsdldos00@gmail.com>
2025-04-12 04:23:42 +0900

    refactor: 불필요한 import 제거

--- src/test/java/com/meongnyangerang/meongnyangerang/service/notification/NotificationServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/notification/NotificationServiceTest.java
@@ -3,7 +3,6 @@ package com.meongnyangerang.meongnyangerang.service.notification;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
