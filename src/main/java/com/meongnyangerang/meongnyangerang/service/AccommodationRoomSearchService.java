17e69223f3b49eb94c7099057d32b9303aaf4f37
JJae <jjae.3.lee@gmail.com>
2025-04-13 04:40:43 +0900

    feat: AccommodationRoomSearchService 에 첫 객실 등록, 이후 객실 등록 & 숙소/객실 수정 시 로직 각각 작성
    - 첫 객실 등록 시, 색인하여 accommodations 인덱스에 문서를 저장
    - 추가 객실 등록 또는 숙소/객실 수정 시, 최소 가격과 반려동물 편의시설을 기존 문서에 업데이트
    - ErrorCode 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/service/AccommodationRoomSearchService.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/service/AccommodationRoomSearchService.java
@@ -6,6 +6,7 @@ import co.elastic.clients.elasticsearch.core.IndexResponse;
 import com.meongnyangerang.meongnyangerang.component.AccommodationRoomMapper;
 import com.meongnyangerang.meongnyangerang.domain.AccommodationRoomDocument;
 import com.meongnyangerang.meongnyangerang.domain.accommodation.Accommodation;
+import com.meongnyangerang.meongnyangerang.domain.accommodation.AccommodationDocument;
 import com.meongnyangerang.meongnyangerang.domain.accommodation.AllowPet;
 import com.meongnyangerang.meongnyangerang.domain.accommodation.facility.AccommodationFacility;
 import com.meongnyangerang.meongnyangerang.domain.accommodation.facility.AccommodationPetFacility;
@@ -13,14 +14,22 @@ import com.meongnyangerang.meongnyangerang.domain.room.Room;
 import com.meongnyangerang.meongnyangerang.domain.room.facility.Hashtag;
 import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomFacility;
 import com.meongnyangerang.meongnyangerang.domain.room.facility.RoomPetFacility;
+import com.meongnyangerang.meongnyangerang.exception.ErrorCode;
+import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
 import com.meongnyangerang.meongnyangerang.repository.accommodation.AccommodationFacilityRepository;
 import com.meongnyangerang.meongnyangerang.repository.accommodation.AccommodationPetFacilityRepository;
 import com.meongnyangerang.meongnyangerang.repository.accommodation.AllowPetRepository;
 import com.meongnyangerang.meongnyangerang.repository.room.HashtagRepository;
 import com.meongnyangerang.meongnyangerang.repository.room.RoomFacilityRepository;
 import com.meongnyangerang.meongnyangerang.repository.room.RoomPetFacilityRepository;
+import com.meongnyangerang.meongnyangerang.repository.room.RoomRepository;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -39,6 +48,7 @@ public class AccommodationRoomSearchService {
   private final AccommodationFacilityRepository accommodationFacilityRepository;
   private final AccommodationPetFacilityRepository accommodationPetFacilityRepository;
   private final AllowPetRepository allowPetRepository;
+  private final RoomRepository roomRepository;
 
   /**
    * 객실 등록 또는 숙소/객실 수정 시 색인 저장
@@ -109,5 +119,96 @@ public class AccommodationRoomSearchService {
     for (Room room : rooms) {
       save(accommodation, room);
     }
+  }
+
+  // 첫 객실 등록 시, 색인하여 accommodations 인덱스에 문서를 저장
+  public void indexAccommodationDocument(Accommodation accommodation, Room room) {
+    Set<String> allowedPetTypes = getAllowedPetTypes(accommodation);
+
+    Set<String> accommodationPetFacilities = getAccommodationPetFacilities(accommodation);
+
+    Set<String> roomPetFacilities = getRoomPetFacilities(room);
+
+    AccommodationDocument document = toDocument(
+        accommodation, room, accommodationPetFacilities, allowedPetTypes, roomPetFacilities);
+
+    try {
+      elasticsearchClient.index(index -> index.index("accommodations")
+          .id(document.getId().toString())
+          .document(document));
+    } catch (IOException e) {
+      throw new MeongnyangerangException(ErrorCode.ELASTICSEARCH_SAVE_FAILED);
+    }
+  }
+
+  // 추가 객실 등록 또는 숙소/객실 수정 시, 최소 가격과 반려동물 편의시설을 기존 문서에 업데이트
+  public void updateAccommodationDocument(Accommodation accommodation) {
+    List<Room> rooms = roomRepository.findAllByAccommodationId(accommodation.getId());
+
+    Set<String> updatedAccommodationPetFacilities = getAccommodationPetFacilities(accommodation);
+    Set<String> updatedRoomPetFacilities = collectRoomPetFacilities(rooms);
+    long minPrice = calculateMinRoomPrice(rooms);
+
+    Map<String, Object> doc = Map.of(
+        "price", minPrice,
+        "accommodationPetFacilities", updatedAccommodationPetFacilities,
+        "roomPetFacilities", updatedRoomPetFacilities
+    );
+
+    try {
+      elasticsearchClient.update(
+          u -> u.index("accommodations")
+              .id(accommodation.getId().toString())
+              .doc(doc),
+          AccommodationDocument.class
+      );
+    } catch (IOException e) {
+      throw new MeongnyangerangException(ErrorCode.ELASTICSEARCH_SAVE_FAILED);
+    }
+  }
+
+  private Set<String> collectRoomPetFacilities(List<Room> rooms) {
+    return rooms.stream()
+        .flatMap(r -> roomPetFacilityRepository.findAllByRoomId(r.getId()).stream())
+        .map(f -> f.getType().name())
+        .collect(Collectors.toSet());
+  }
+
+  private long calculateMinRoomPrice(List<Room> rooms) {
+    return rooms.stream()
+        .map(Room::getPrice)
+        .min(Long::compareTo).orElse(0L);
+  }
+
+  private AccommodationDocument toDocument(Accommodation accommodation,
+      Room room, Set<String> accommodationPetFacilities, Set<String> allowedPetTypes,
+      Set<String> roomPetFacilities) {
+    return AccommodationDocument.builder()
+        .id(accommodation.getId())
+        .name(accommodation.getName())
+        .thumbnailUrl(accommodation.getThumbnailUrl())
+        .price(room.getPrice())
+        .totalRating(accommodation.getTotalRating())
+        .accommodationPetFacilities(accommodationPetFacilities)
+        .allowedPetTypes(allowedPetTypes)
+        .roomPetFacilities(roomPetFacilities)
+        .build();
+  }
+
+  private Set<String> getRoomPetFacilities(Room room) {
+    return roomPetFacilityRepository.findAllByRoomId(room.getId())
+        .stream().map(RoomPetFacility::getType).map(Enum::name).collect(Collectors.toSet());
+  }
+
+  private Set<String> getAccommodationPetFacilities(Accommodation accommodation) {
+    return accommodationPetFacilityRepository.findAllByAccommodationId(
+            accommodation.getId()).stream().map(AccommodationPetFacility::getType).map(Enum::name)
+        .collect(Collectors.toSet());
+  }
+
+  private Set<String> getAllowedPetTypes(Accommodation accommodation) {
+    return allowPetRepository.findAllByAccommodationId(
+            accommodation.getId()).stream().map(AllowPet::getPetType).map(Enum::name)
+        .collect(Collectors.toSet());
   }
 }
