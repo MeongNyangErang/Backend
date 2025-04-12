1d65c5ce22bd569e42bc682fe56fbc5273805858
seokjin <wlsdldos00@gmail.com>
2025-04-13 00:06:21 +0900

    test: 알림 삭제 테스트 코드 구현

--- src/test/java/com/meongnyangerang/meongnyangerang/service/notification/NotificationServiceTest.java
+++ src/test/java/com/meongnyangerang/meongnyangerang/service/notification/NotificationServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -286,6 +287,37 @@ class NotificationServiceTest {
     verify(notificationRepository).findAllByHost_IdOrderByCreatedAtDesc(host.getId(), pageable);
   }
 
+  @Test
+  @DisplayName("알림 삭제 - 사용자")
+  void deleteNotification_AsUser_Success() {
+    // given
+    Long userId = user.getId();
+    Long notificationId = 1L;
+    Notification notification = createNotificationReceiveByUser(
+        notificationId, user, "사용자 알림");
+
+    // when
+    notificationService.deleteNotificationAsUser(notification.getId(), userId);
+
+    // then
+    verify(notificationRepository).deleteByIdAndUser_Id(notificationId, userId);
+  }
+
+  @Test
+  @DisplayName("알림 삭제 - 호스트")
+  void deleteNotification_AsHost_Success() {
+    // given
+    Long hostId = host.getId();
+    Long notificationId = 1L;
+    Notification notification = createNotificationReceiveByHost(
+        notificationId, host, "호스트 알림");
+
+    // when
+    notificationService.deleteNotificationAsHost(notification.getId(), hostId);
+
+    // then
+    verify(notificationRepository).deleteByIdAndHost_Id(notificationId, hostId);
+  }
 
   private Notification createNotificationReceiveByHost(Long id, Host host, String content) {
     return Notification.builder()
