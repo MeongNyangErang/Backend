67f6e02c4ab3fbf2fa04ec2654ba04106887896b
Ho-JunYu <hojoon1011@gmail.com>
2025-04-13 23:10:51 +0900

    feat: CORS 설정에 프론트엔드 주소 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/config/WebConfig.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("*") // 또는 프론트엔드 실제 주소만
+        .allowedOrigins("http://localhost:5173/") // 또는 프론트엔드 실제 주소만
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         .allowCredentials(true);
   }
