d84a8b0c9c1c821b03e6555bdc42da0aa7d6640c
Ho-JunYu <hojoon1011@gmail.com>
2025-04-14 22:20:52 +0900

    chore:불필요한 import 제거

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/HostController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/HostController.java
@@ -1,5 +1,6 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
+import com.meongnyangerang.meongnyangerang.dto.HostPhoneUpdateRequest;
 import com.meongnyangerang.meongnyangerang.dto.HostProfileResponse;
 import com.meongnyangerang.meongnyangerang.dto.HostSignupRequest;
 import com.meongnyangerang.meongnyangerang.dto.LoginRequest;
--- src/main/java/com/meongnyangerang/meongnyangerang/dto/HostPhoneUpdateRequest.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/HostPhoneUpdateRequest.java
@@ -2,8 +2,6 @@ package com.meongnyangerang.meongnyangerang.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 public record HostPhoneUpdateRequest(
     @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "유효하지 않은 전화번호 형식입니다")
