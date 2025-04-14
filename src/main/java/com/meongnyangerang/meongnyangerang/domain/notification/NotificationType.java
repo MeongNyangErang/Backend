29a7267d643e9be68dfcf9353d8b389550c37120
seokjin <wlsdldos00@gmail.com>
2025-04-14 21:03:24 +0900

    commnet: 각 알림 타입에 누구에게 보내는 타입인지 주석 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/domain/notification/NotificationType.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/notification/NotificationType.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum NotificationType {
 
   MESSAGE("메세지"),
-  RESERVATION_CONFIRMED("예약 확인"), // 사용자에게
-  RESERVATION_REGISTERED("예약 등록"), // 호스트에게
-  RESERVATION_REMINDER("예약 리마인더"),
+  RESERVATION_CONFIRMED("예약 확인"),    // 사용자에게
+  RESERVATION_REGISTERED("예약 등록"),  // 호스트에게
+  RESERVATION_REMINDER("예약 리마인더"), // 사용자에게
   REVIEW("리뷰");
 
   private final String value;
