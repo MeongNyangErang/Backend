56c4d1daa493a0c57db4ab6e29683fe73986f54d
Ho-JunYu <hojoon1011@gmail.com>
2025-04-14 22:20:10 +0900

    refactor: HostPhoneUpdateRequest를 record로 변경

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/HostPhoneUpdateRequest.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/HostPhoneUpdateRequest.java
@@ -5,12 +5,10 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class HostPhoneUpdateRequest {
+public record HostPhoneUpdateRequest(
+    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "유효하지 않은 전화번호 형식입니다")
+    @NotBlank(message = "새로운 전화번호를 입력하세요.")
+    String phoneNumber
+) {}
 
-  @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "유효하지 않은 전화번호 형식입니다")
-  @NotBlank(message = "새로운 전화번호를 입력하세요.")
-  private String phoneNumber;
 
-}
