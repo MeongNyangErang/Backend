4a75b5a58fea5e7350a038bb6e56a6a32a450375
JJae <jjae.3.lee@gmail.com>
2025-04-16 14:09:13 +0900

    refactor: 로그인 사용자 숙소 추천 response 에 petId 포함 시키기 위한 dto 생성

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/accommodation/PetRecommendationGroup.java
@@ -0,0 +1,13 @@
+package com.meongnyangerang.meongnyangerang.dto.accommodation;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PetRecommendationGroup {
+  private Long petId;
+  private String petName;
+  private List<RecommendationResponse> recommendations;
+}
