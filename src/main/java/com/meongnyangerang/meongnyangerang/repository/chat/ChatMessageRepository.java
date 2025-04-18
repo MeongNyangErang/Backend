1342a422f458ec1cfbb95232942836a99204be52
seokjin <wlsdldos00@gmail.com>
2025-04-19 03:52:29 +0900

    feat: 읽지 않은 메시지 수를 조회하는 쿼리 메서드를 JPA 명명규칙으로 재정의

--- src/main/java/com/meongnyangerang/meongnyangerang/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/repository/chat/ChatMessageRepository.java
@@ -6,8 +6,6 @@ import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,14 +14,10 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
   // 마지막 메시지 조회
   ChatMessage findTopByChatRoomIdOrderByCreatedAtDesc(Long roomId);
 
-  // 읽지 않은 메시지 수 조회
-  @Query("SELECT COUNT(cm) FROM ChatMessage cm " +
-      "WHERE cm.chatRoom.id = :roomId " +
-      "AND cm.senderType = :senderType " +
-      "AND cm.createdAt > :lastReadTime")
-  int countUnreadMessages(@Param("roomId") Long roomId,
-      @Param("senderType") SenderType senderType,
-      @Param("lastReadTime") LocalDateTime lastReadTime
+  int countByChatRoomIdAndSenderTypeAndCreatedAtGreaterThan(
+      Long roomId,
+      SenderType senderType,
+      LocalDateTime lastReadTime
   );
 
   Page<ChatMessage> findByChatRoomId(Long chatRoomId, Pageable pageable);
