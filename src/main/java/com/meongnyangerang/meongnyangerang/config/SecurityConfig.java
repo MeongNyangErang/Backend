4b590fd497c70a3fe75e86aca4e6a679122ef11a
Ho-JunYu <hojoon1011@gmail.com>
2025-04-21 04:18:25 +0900

    fix: SockJS handshake 요청(`/ws/info`)에서 발생하던 401 오류 해결
    - Spring Security에서 `/ws/info` 명시적으로 permitAll 추가
    - SockJS의 WebSocket 핸드셰이크 사전 요청을 허용하지 않아 발생하던 문제 수정

--- src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 "/api/v1/admin/login",
                 "/api/v1/recommendations/default/**",
                 "/ws/**",
+                "/ws/info",
                 "/api/v1/accommodations/{accommodationId}/reviews",
                 "/api/v1/search/accommodations",
                 "/api/v1/accommodations/{accommodationId}",
