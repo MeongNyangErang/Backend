bd6c5bd68041161cad1cc47ca32620d672e5d966
JJae <jjae.3.lee@gmail.com>
2025-04-22 03:10:57 +0900

    feat: 리뷰 신고 요청 생성
    - toEntity 추가

---
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/ReviewReportRequest.java
@@ -0,0 +1,33 @@
+package com.meongnyangerang.meongnyangerang.dto;
+
+import com.meongnyangerang.meongnyangerang.domain.review.ReportStatus;
+import com.meongnyangerang.meongnyangerang.domain.review.ReporterType;
+import com.meongnyangerang.meongnyangerang.domain.review.Review;
+import com.meongnyangerang.meongnyangerang.domain.review.ReviewReport;
+import com.meongnyangerang.meongnyangerang.domain.user.Role;
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewReportRequest {
+
+  private String reason;
+
+  public ReviewReport toEntity(UserDetailsImpl userDetails, Review review,
+      String evidenceImageUrl) {
+    return ReviewReport.builder()
+        .review(review)
+        .reporterId(userDetails.getId())
+        .type(userDetails.getRole() == Role.ROLE_USER ? ReporterType.USER : ReporterType.HOST)
+        .reason(this.reason)
+        .evidenceImageUrl(evidenceImageUrl == null ? "" : evidenceImageUrl)
+        .status(ReportStatus.PENDING)
+        .build();
+  }
+}
