fb339b5c10bf3419d3d5386d661db56f73e69c5c
seokjin <wlsdldos00@gmail.com>
2025-04-21 22:49:14 +0900

    feat: 액추에이터 security 설정

--- src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/config/SecurityConfig.java
@@ -54,7 +54,8 @@ public class SecurityConfig {
                 "/api/v1/accommodations/{accommodationId}/reviews",
                 "/api/v1/search/accommodations",
                 "/api/v1/accommodations/{accommodationId}",
-                "/api/v1/rooms/{roomId}"
+                "/api/v1/rooms/{roomId}",
+                "/actuator/**"
             ).permitAll()
             .requestMatchers("/api/v1/users/**").hasAuthority("ROLE_USER")
             .requestMatchers("/api/v1/chats/users/create").hasAuthority("ROLE_USER")
