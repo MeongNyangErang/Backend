d9d64f141e046d1f766a46038a02f97c9b660892
Ho-JunYu <hojoon1011@gmail.com>
2025-04-22 20:28:50 +0900

    feat: 숙소 상세 조회 API에 @AuthenticationPrincipal 파라미터 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/controller/PublicAccommodationController.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/controller/PublicAccommodationController.java
@@ -1,9 +1,11 @@
 package com.meongnyangerang.meongnyangerang.controller;
 
 import com.meongnyangerang.meongnyangerang.dto.accommodation.AccommodationDetailResponse;
+import com.meongnyangerang.meongnyangerang.security.UserDetailsImpl;
 import com.meongnyangerang.meongnyangerang.service.AccommodationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,7 @@ public class PublicAccommodationController {
   // 숙소 상세 조회 API (모든 유저가 사용 가능)
   @GetMapping("/{accommodationId}")
   public ResponseEntity<AccommodationDetailResponse> getAccommodationDetail(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
       @PathVariable Long accommodationId) {
 
     return ResponseEntity.ok(accommodationService.getAccommodationDetail(accommodationId));
