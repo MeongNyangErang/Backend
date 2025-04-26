1e520d357e21c7483e45951098a038077cc17a45
seokjin <wlsdldos00@gmail.com>
2025-04-26 09:02:32 +0900

    feat: 역할에 맞는 SenderType을 반환하는 메서드 구현

--- src/main/java/com/meongnyangerang/meongnyangerang/domain/user/Role.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/domain/user/Role.java
@@ -1,7 +1,19 @@
 package com.meongnyangerang.meongnyangerang.domain.user;
 
+import com.meongnyangerang.meongnyangerang.domain.chat.SenderType;
+import com.meongnyangerang.meongnyangerang.exception.ErrorCode;
+import com.meongnyangerang.meongnyangerang.exception.MeongnyangerangException;
+
 public enum Role {
   ROLE_USER,
   ROLE_HOST,
-  ROLE_ADMIN
+  ROLE_ADMIN;
+
+  public SenderType toSenderType() {
+    return switch (this) {
+      case ROLE_USER -> SenderType.USER;
+      case ROLE_HOST -> SenderType.HOST;
+      default -> throw new MeongnyangerangException(ErrorCode.INVALID_AUTHORIZED);
+    };
+  }
 }
