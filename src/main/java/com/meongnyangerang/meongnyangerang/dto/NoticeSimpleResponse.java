817501077ba0211328fc18c49c9e46bd039af6f0
Ho-JunYu <hojoon1011@gmail.com>
2025-05-08 04:16:52 +0900

    feat: NoticeSimpleResponse에 Notice 엔티티 변환용 from 메서드 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/NoticeSimpleResponse.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/NoticeSimpleResponse.java
@@ -1,9 +1,19 @@
 package com.meongnyangerang.meongnyangerang.dto;
 
+import com.meongnyangerang.meongnyangerang.domain.admin.Notice;
 import java.time.LocalDateTime;
 
 public record NoticeSimpleResponse (
     Long noticeId,
     String title,
     LocalDateTime createdAt
-) {}
+) {
+
+  public static NoticeSimpleResponse from(Notice notice) {
+    return new NoticeSimpleResponse(
+        notice.getId(),
+        notice.getTitle(),
+        notice.getCreatedAt()
+    );
+  }
+}
