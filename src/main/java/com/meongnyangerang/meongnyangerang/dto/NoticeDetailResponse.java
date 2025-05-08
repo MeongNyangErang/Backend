4d080e834da8754462ab318adc6cfd176cb7565b
Ho-JunYu <hojoon1011@gmail.com>
2025-05-09 02:51:53 +0900

    feat: NoticeDetailResponse에 Notice 엔티티 변환용 from 메서드 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/NoticeDetailResponse.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/NoticeDetailResponse.java
@@ -1,5 +1,6 @@
 package com.meongnyangerang.meongnyangerang.dto;
 
+import com.meongnyangerang.meongnyangerang.domain.admin.Notice;
 import java.time.LocalDateTime;
 
 public record NoticeDetailResponse (
@@ -8,4 +9,15 @@ public record NoticeDetailResponse (
     String content,
     String noticeImageUrl,
     LocalDateTime createdAt
-) {}
+) {
+
+  public static NoticeDetailResponse from(Notice notice) {
+    return new NoticeDetailResponse(
+        notice.getId(),
+        notice.getTitle(),
+        notice.getContent(),
+        notice.getImageUrl(),
+        notice.getCreatedAt()
+    );
+  }
+}
