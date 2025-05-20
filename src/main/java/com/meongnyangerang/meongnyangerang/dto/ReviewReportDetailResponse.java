8210ef12a72e94d79428b4cc6ae4e2335b7cc77a
JJae <jjae.3.lee@gmail.com>
2025-05-20 11:05:11 +0900

    feat: ReviewReportDetailResponse 에 from 메서드 추가

--- src/main/java/com/meongnyangerang/meongnyangerang/dto/ReviewReportDetailResponse.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/dto/ReviewReportDetailResponse.java
@@ -1,5 +1,7 @@
 package com.meongnyangerang.meongnyangerang.dto;
 
+import com.meongnyangerang.meongnyangerang.domain.review.ReviewReport;
+import java.time.format.DateTimeFormatter;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,4 +16,17 @@ public class ReviewReportDetailResponse {
   private String evidenceImageUrl;
   private String reportDate;
 
+  public static ReviewReportDetailResponse from(ReviewReport reviewReport,
+      String reporterNickname) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    return ReviewReportDetailResponse.builder()
+        .reviewId(reviewReport.getReview().getId())
+        .reviewerNickname(reviewReport.getReviewerNickname())
+        .reporterNickname(reporterNickname)
+        .reason(reviewReport.getReason())
+        .evidenceImageUrl(reviewReport.getEvidenceImageUrl())
+        .reportDate(reviewReport.getCreatedAt().format(formatter))
+        .build();
+  }
 }
