68fe0de458a5928aad63c9174c0698d3c040f815
Ho-JunYu <hojoon1011@gmail.com>
2025-06-02 00:59:29 +0900

    style: WishlistService 클래스 코드 포맷팅 및 불필요한 import 정리
    - 코드 일관성 및 가독성 향상

--- src/main/java/com/meongnyangerang/meongnyangerang/service/WishlistService.java
+++ src/main/java/com/meongnyangerang/meongnyangerang/service/WishlistService.java
@@ -15,9 +15,7 @@ import com.meongnyangerang.meongnyangerang.repository.UserRepository;
 import com.meongnyangerang.meongnyangerang.repository.WishlistRepository;
 import com.meongnyangerang.meongnyangerang.repository.accommodation.AccommodationRepository;
 import java.util.Collections;
-import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -97,12 +95,15 @@ public class WishlistService {
 
   // 찜 여부 확인
   public boolean isWishlisted(Long userId, Long accommodationId) {
-    return Boolean.TRUE.equals(redisTemplate.opsForSet().isMember(getWishlistKey(userId), accommodationId));
+    return Boolean.TRUE.equals(
+        redisTemplate.opsForSet().isMember(getWishlistKey(userId), accommodationId));
   }
 
   // Redis에서 해당 사용자가 찜한 숙소 ID 목록을 조회
   public Set<Long> getWishlistIdsFromRedis(Long userId) {
-    if (userId == null) return Collections.emptySet();
+    if (userId == null) {
+      return Collections.emptySet();
+    }
 
     String key = getWishlistKey(userId);
     Set<Long> redisIds = redisTemplate.opsForSet().members(key);
